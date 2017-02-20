84d0c2c2b30f528456b0345692cd86e437359e97
alexpassss <alexpassss@gmail.com>
2017-02-21 01:00:43 +0200

    Connected Sails app with MongoDB

---
+++ api/models/Use.js
@@ -0,0 +1,19 @@
+/**
+ * Use
+ *
+ * @module      :: Model
+ * @description :: Tracks the usage of a given Jwt
+ * @docs        :: http://waterlock.ninja/documentation
+ */
+
+module.exports = {
+
+  attributes: require('waterlock').models.use.attributes({
+    remoteAddress: {
+      type: 'string'
+    },
+    jsonWebToken: {
+      model: 'jwt'
+    }
+  })
+};
---
+++ api/models/User.js
@@ -0,0 +1,27 @@
+/**
+ * User
+ *
+ * @module      :: Model
+ * @description :: This is the base user model
+ * @docs        :: http://waterlock.ninja/documentation
+ */
+
+module.exports = {
+
+  attributes: require('waterlock').models.user.attributes({
+    attempts: {
+        collection: 'attempt',
+        via: 'user'
+    },
+    jsonWebTokens: {
+        collection: 'jwt',
+        via: 'owner'
+    },
+    auth:{
+        model: 'auth'
+    }
+  }),
+
+  beforeCreate: require('waterlock').models.user.beforeCreate,
+  beforeUpdate: require('waterlock').models.user.beforeUpdate
+};
